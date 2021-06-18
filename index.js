fdb3723bdf2aba6bae480daea3f930fe198f36e0
S M Kaiser Ahmed <53963906+mekaiser@users.noreply.github.com>
2021-06-19 04:20:35 +0600

    done

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+
 // const rootCall = (req, res) => res.send('Thank you very much');
 
-app.get('/', (req, res) => {
-    res.send("I know how to open Node...YAY");
+const users = ["Arid", "Moin", "Sabed", "Sushmita", "Shohana", "Salman"];
+
+app.get("/", (req, res) => {
+  const fruit = {
+    product: "ada",
+    price: 220,
+  };
+  res.send(fruit);
+});
+
+// app.get("/fruits/banana", (req, res) => {
+//   res.send({ fruit: "banana", quantity: 1000, price: 10000 });
+// });
+
+// get
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(req.query.sort);
+  const name = users[id];
+  res.send({ id, name });
+});
+
+// post
+app.post('/addUser', (req, res) => {
+    //save to database
+    const user = req.body;
+    user.id = 46;
+    res.send(user);
 })
 
-app.listen(3000, () => console.log('listening to port 3000'));
+app.listen(4200, () => console.log("listening to port 4200"));
